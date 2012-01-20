4099e103d9f4f0c8ec5d8429efef4f290981c301
Naveen LP <naveenlp@gmail.com>
2012-01-20 17:19:29 +0800

    made some changes to activity to allow for alerts to be processed

--- Android Package/Mobdis_Export_Android/src/com/android/app/MainActivity.java
+++ Android Package/Mobdis_Export_Android/src/com/android/app/MainActivity.java
@@ -10,6 +10,7 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+import com.company.watertown.R;
 
 public class MainActivity extends Activity {
 	private WebView wView;
@@ -33,34 +34,12 @@ public class MainActivity extends Activity {
 	    
 	    wView.loadUrl("file:///android_asset/Server/index.html");
 	    
-//      wView.loadUrl("http://appointment.mobdis.co");
-//	    wView.getSettings().setSupportZoom(true);
-//	    wView.getSettings().setBuiltInZoomControls(true);
-	    
-//	    wView.setWebViewClient(new WebViewClient() {
-//	    	@Override
-//	    	public boolean shouldOverrideUrlLoading (WebView view, String url) {
-//	    		Log.d("test","test: 1: "+url);
-////	    		if(!url.contains("http://fareast.com.sg/watertown"))
-////	    			return true;
-////	    		Log.d("test","test: "+url);
-////	    		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-////	    		startActivity(browserIntent);
-//	    		
-//	    		return false;
-//	    	}
-//	    });
-	    
-	    //wView.loadUrl("http://ilovewatertown.com.sg");
-//	    wView.loadUrl("http://jquerymobile.com");
 	    wView.setWebChromeClient(new WebChromeClient() {
 	    	@Override
-	    	public boolean onJsAlert(WebView view, String browser_url, String url, JsResult result) {
+	    	public boolean onJsAlert(WebView view, String browser_url, String message, JsResult result) {
 	    		//Required functionality here
-	    		Log.d("test","test url: "+url);
-	    		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-	    		startActivity(browserIntent);
-	    		return true;
+//	    		Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT);
+	    		return super.onJsAlert(view, browser_url, message, result);
 	        }
 	    }); 
 	  

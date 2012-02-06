3cfaea00e461e8870d497edd93ec09a38ab70f0f
Naveen LP <naveenlp@gmail.com>
2012-02-06 15:31:18 +0800

    Have rolled-back to initial commit, then changed the MainActivity.java to include the java script interface and finally, have changed the package name to avoid errors. Also, for each new project, the package name will have to be separately changed. Right click on project -> android tools -> rename application package

--- Android Package/Mobdis_Export_Android/src/com/android/app/MainActivity.java
+++ Android Package/Mobdis_Export_Android/src/com/android/app/MainActivity.java
@@ -8,55 +8,49 @@ import android.util.Log;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
-import com.company.watertown.R;
+import com.company2359.mobdisproject.R;
 
 public class MainActivity extends Activity {
 	private WebView wView;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-//        Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.fromFile(new File("/mnt/sdcard/Server/index.html")));// Uri.parse("content://com.android.htmlfileprovider/sdcard/Server/index.html"));
-//        browserIntent.setClassName("com.android.browser", "com.android.browser.BrowserActivity");
-//		startActivity(browserIntent);
-        
-        setContentView(R.layout.webview_layout);
-         
-        wView = (WebView) findViewById(R.id.webview);
-        wView.getSettings().setJavaScriptEnabled(true);
-        wView.setBackgroundColor(0);
-        wView.getSettings().setLoadsImagesAutomatically(true);
-        wView.clearCache(true);
-	    
-	    wView.loadUrl("file:///android_asset/Server/index.html");
-	    
-	    wView.setWebChromeClient(new WebChromeClient() {
-	    	@Override
-	    	public boolean onJsAlert(WebView view, String browser_url, String message, JsResult result) {
-	    		//Required functionality here
-//	    		Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT);
-	    		return super.onJsAlert(view, browser_url, message, result);
-	        }
-	    }); 
-	  
-	    wView.addJavascriptInterface(new IJavascriptHandler(), "communication");
-	    
-    }
-    
-    final class IJavascriptHandler {
-    	   IJavascriptHandler() {
-    	   }
 
-    	   public void sendToAndroid(String url) {
-    	      // this is called from JS with passed value
-    		   Log.d("test","test urlb: "+url);
-    		   Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-    		   startActivity(browserIntent);
-    	   }
-    }
-    
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.webview_layout);
+
+		wView = (WebView) findViewById(R.id.webview);
+		wView.getSettings().setJavaScriptEnabled(true);
+		wView.setBackgroundColor(0);
+		wView.getSettings().setLoadsImagesAutomatically(true);
+		wView.clearCache(true);
+
+		wView.loadUrl("file:///android_asset/Server/index.html");
+
+		wView.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public boolean onJsAlert(WebView view, String browser_url, String message, JsResult result) {
+				//Required functionality here
+				// Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT);
+				return super.onJsAlert(view, browser_url, message, result);
+			}
+		});
+
+		wView.addJavascriptInterface(new IJavascriptHandler(), "communication");
+
+	}
+
+	final class IJavascriptHandler {
+		IJavascriptHandler() {
+		}
+
+		public void sendToAndroid(String url) {
+			// this is called from JS with passed value
+			Log.d("test","test urlb: "+url);
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+			startActivity(browserIntent);
+		}
+	}
+
 }
